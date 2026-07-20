e8513fd50314c1426b2074bf03fe2b3c492439bf
Vojtech Pejsar <pejsavo3@fit.cvut.cz>
2026-07-20 02:24:04 +0200

    Add food domain model

---
+++ src/backend/Icebox.Domain/Foods/Food.cs
@@ -0,0 +1,23 @@
+namespace Icebox.Domain.Foods;
+
+public class Food
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public DateTime ExpirationDate { get; private set; }
+    public Guid FridgeId { get; private set; }
+
+    public Food(string name, DateTime expirationDate, Guid fridgeId)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Food name cannot be empty.", nameof(name));
+        
+        if (fridgeId == Guid.Empty)
+            throw new ArgumentException("Fridge ID must be provided.", nameof(fridgeId));
+
+        Id = Guid.NewGuid();
+        Name = name;
+        ExpirationDate = expirationDate;
+        FridgeId = fridgeId;
+    }
+}
