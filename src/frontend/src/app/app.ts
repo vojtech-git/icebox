ee964d05c6bd577154772f1afbf7f20dc9e75587
Vojtech Pejsar <pejsarvojtech@gmail.com>
2026-08-28 16:50:43 +0200

    Change template import in app component

--- src/frontend/src/app/app.ts
+++ src/frontend/src/app/app.ts
@@ -4,7 +4,7 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  template: `<router-outlet />`,
+  templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {}
