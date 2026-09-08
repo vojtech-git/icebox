bd5627d32a02866fa8ee2ccc871899c5e10c47b5
Vojtech Pejsar <pejsarvojtech@gmail.com>
2026-09-08 23:14:10 +0200

    Add cancelation tokens to endpoint

--- src/backend/Icebox.API/Foods/FoodController.cs
+++ src/backend/Icebox.API/Foods/FoodController.cs
@@ -9,34 +9,33 @@ namespace Icebox.API.Foods;
 public class FoodController : ControllerBase
 {
   private readonly IMediator _mediator;
-
   public FoodController(IMediator mediator) => _mediator = mediator;
 
   [HttpPost]
-  public async Task<IActionResult> Create([FromBody] CreateFoodRequest request)
+  public async Task<IActionResult> Create([FromBody] CreateFoodRequest request, CancellationToken cancellationToken)
   {
-    var result = await _mediator.Send(new CreateFoodCommand(request.Name, request.ExpirationDate, request.FridgeId));
+    var result = await _mediator.Send(new CreateFoodCommand(request.Name, request.ExpirationDate, request.FridgeId), cancellationToken);
     return result is null ? BadRequest("Target fridge does not exist.") : CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
   }
 
   [HttpGet("{id:guid}")]
-  public async Task<IActionResult> GetById(Guid id)
+  public async Task<IActionResult> GetById(Guid id, CancellationToken cancellationToken)
   {
-    var result = await _mediator.Send(new GetFoodByIdQuery(id));
+    var result = await _mediator.Send(new GetFoodByIdQuery(id), cancellationToken);
     return result is null ? NotFound() : Ok(result);
   }
 
   [HttpPatch("{id:guid}")]
-  public async Task<IActionResult> Update(Guid id, [FromBody] UpdateFoodRequest request)
+  public async Task<IActionResult> Update(Guid id, [FromBody] UpdateFoodRequest request, CancellationToken cancellationToken)
   {
-    var result = await _mediator.Send(new UpdateFoodCommand(id, request.Name, request.ExpirationDate));
+    var result = await _mediator.Send(new UpdateFoodCommand(id, request.Name, request.ExpirationDate), cancellationToken);
     return result is null ? NotFound() : Ok(result);
   }
 
   [HttpDelete("{id:guid}")]
-  public async Task<IActionResult> Delete(Guid id)
+  public async Task<IActionResult> Delete(Guid id, CancellationToken cancellationToken)
   {
-    var success = await _mediator.Send(new DeleteFoodCommand(id));
+    var success = await _mediator.Send(new DeleteFoodCommand(id), cancellationToken);
     return success ? NoContent() : NotFound();
   }
 }
--- src/backend/Icebox.API/Fridges/FridgeController.cs
+++ src/backend/Icebox.API/Fridges/FridgeController.cs
@@ -9,44 +9,40 @@ namespace Icebox.API.Fridges;
 public class FridgeController : ControllerBase
 {
   private readonly IMediator _mediator;
-
-  public FridgeController(IMediator mediator)
-  {
-    _mediator = mediator;
-  }
+  public FridgeController(IMediator mediator) => _mediator = mediator;
 
   [HttpPost]
-  public async Task<IActionResult> Create([FromBody] CreateFridgeRequest request)
+  public async Task<IActionResult> Create([FromBody] CreateFridgeRequest request, CancellationToken cancellationToken)
   {
-    var result = await _mediator.Send(new CreateFridgeCommand(request.Name));
+    var result = await _mediator.Send(new CreateFridgeCommand(request.Name), cancellationToken);
     return CreatedAtAction(nameof(GetById), new { id = result.Id }, result);
   }
 
   [HttpGet]
-  public async Task<IActionResult> GetAll()
+  public async Task<IActionResult> GetAll(CancellationToken cancellationToken)
   {
-    var result = await _mediator.Send(new GetAllFridgesQuery());
+    var result = await _mediator.Send(new GetAllFridgesQuery(), cancellationToken);
     return Ok(result);
   }
 
   [HttpGet("{id:guid}")]
-  public async Task<IActionResult> GetById(Guid id)
+  public async Task<IActionResult> GetById(Guid id, CancellationToken cancellationToken)
   {
-    var result = await _mediator.Send(new GetFridgeByIdQuery(id));
+    var result = await _mediator.Send(new GetFridgeByIdQuery(id), cancellationToken);
     return result is null ? NotFound() : Ok(result);
   }
 
   [HttpPatch("{id:guid}")]
-  public async Task<IActionResult> Update(Guid id, [FromBody] UpdateFridgeRequest request)
+  public async Task<IActionResult> Update(Guid id, [FromBody] UpdateFridgeRequest request, CancellationToken cancellationToken)
   {
-    var result = await _mediator.Send(new UpdateFridgeCommand(id, request.Name));
+    var result = await _mediator.Send(new UpdateFridgeCommand(id, request.Name), cancellationToken);
     return result is null ? NotFound() : Ok(result);
   }
 
   [HttpDelete("{id:guid}")]
-  public async Task<IActionResult> Delete(Guid id)
+  public async Task<IActionResult> Delete(Guid id, CancellationToken cancellationToken)
   {
-    var success = await _mediator.Send(new DeleteFridgeCommand(id));
+    var success = await _mediator.Send(new DeleteFridgeCommand(id), cancellationToken);
     return success ? NoContent() : NotFound();
   }
 }
