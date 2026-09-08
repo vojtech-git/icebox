9c66c799f91d0b5e5ed977e9fcf4b90abab25ca7
Vojtech Pejsar <pejsarvojtech@gmail.com>
2026-09-09 00:40:27 +0200

    Remove get all from repo

--- src/backend/Icebox.Domain/Fridges/IFridgeRepository.cs
+++ src/backend/Icebox.Domain/Fridges/IFridgeRepository.cs
@@ -5,7 +5,6 @@ namespace Icebox.Domain.Fridges;
 public interface IFridgeRepository
 {
   Task AddAsync(Fridge fridge, CancellationToken cancellationToken);
-  Task<List<Fridge>> GetAllAsync(CancellationToken cancellationToken);
   Task<Fridge?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
   Task DeleteAsync(Fridge fridge, CancellationToken cancellationToken);
   Task SaveChangesAsync(CancellationToken cancellationToken);
